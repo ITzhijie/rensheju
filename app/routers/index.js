381383931774a657460c6c3e438d240df5350c10
xuzhijie <354550860@qq.com>
2021-12-10 09:02:10 +0800

    增加注册和忘记密码页面

--- app/routers/index.js
+++ app/routers/index.js
@@ -5,6 +5,9 @@ module.exports = app => {
   const { router, controller } = app;
 
   router.get('/', controller.admin.home.index);
+  router.get('/register', controller.admin.home.register);
+  router.get('/forget', controller.admin.home.forget);
+
   router.post('/import', controller.home.importExcel);
   router.get('/export', controller.home.exportExcel);
 
