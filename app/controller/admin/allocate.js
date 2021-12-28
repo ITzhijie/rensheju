6b29b454ad4dfba91151dd7897e9e1ed69047f0e
ITzhijie <354550860@qq.com>
2021-12-28 23:43:02 +0800

    完成已分配、已结束列表页

--- app/controller/admin/allocate.js
+++ app/controller/admin/allocate.js
@@ -5,7 +5,7 @@ var BaseController = require('./base.js');
 class Controller extends BaseController {
     //待分配
     async allocating() {
-        // 超过缴费结束时间pay_end  room_status 缴费状态为0
+        // 超过缴费结束时间pay_end  room_status 分配状态为0
         let exam_id =this.ctx.request.query.exam_id?this.app.mongoose.Types.ObjectId(this.ctx.request.query.exam_id) : "";
         let classify_id =this.ctx.request.query.classify_id?this.app.mongoose.Types.ObjectId(this.ctx.request.query.classify_id) : "";
 
@@ -114,7 +114,7 @@ class Controller extends BaseController {
         }
 
         var page = this.ctx.request.query.page || 1;
-        var pageSize = 2;
+        var pageSize = 10;
         var allLists =  await this.ctx.model.Classify.aggregate([
             findJson,
             findJson1,
@@ -155,19 +155,249 @@ class Controller extends BaseController {
     
     //已分配
     async allocated() {
+        // 少于考试结束时间 exam_end  room_status 分配状态为1
+        let exam_id = this.ctx.request.query.exam_id ? this.app.mongoose.Types.ObjectId(this.ctx.request.query.exam_id) : "";
+        let classify_id = this.ctx.request.query.classify_id ? this.app.mongoose.Types.ObjectId(this.ctx.request.query.classify_id) : "";
 
-		await this.ctx.render('admin/allocate/allocated', {
-			
-		});
+        let examLists, classifyLists;
+
+        let findJson1 = {
+            $lookup: {
+                from: 'exam',
+                localField: 'exam_id',
+                foreignField: '_id',
+                as: 'exam'
+            }
+        }
+
+        var findJson2 = {
+            $lookup: {
+                from: "examinee",
+                let: { classify_id: "$_id" },
+                pipeline: [
+                    {
+                        $match:
+                        {
+                            verify_status: 1,
+                            pay_status: 1,
+                            room_status: 1,
+                            $expr: {
+                                $and:
+                                    [
+                                        { $eq: ["$classify_id", "$$classify_id"] }
+                                    ]
+                            },
+                            // $or: [
+                            //     { "brand_name": { "$regex": keyword } },
+                            // ]
+                        }
+                    },
+                    // { $group: { _id: "$pay_status", total: { $sum: "$num" } } }
+                ],
+                as: "examinee_roomed"
+            },
+
+        };
+
+        let matchJson = {
+            $match: {
+                room_status: 1,
+                exam_end: { $gt: new Date() }
+            }
+        }
+        if (exam_id) {
+            matchJson.$match.exam_id = exam_id;
+        }
+        if (classify_id) {
+            matchJson.$match._id = classify_id;
+        }
+        if (this.ctx.session.adminInfo.is_super == 1) {
+            examLists = await this.ctx.model.Exam.find();
+            //classifyLists = await this.ctx.model.Classify.find();
+
+        } else {
+            let admin_organ_id = this.ctx.session.adminInfo.organ_id;
+            examLists = await this.ctx.model.Exam.find({ organ_id: admin_organ_id });
+            //classifyLists = await this.ctx.model.Classify.find({organ_id:admin_organ_id});
+
+            matchJson.$match.organ_id = this.app.mongoose.Types.ObjectId(admin_organ_id);
+
+
+        }
+        if (exam_id) {
+            classifyLists = await this.ctx.model.Classify.find({ exam_id: exam_id });
+        } else {
+            if (this.ctx.session.adminInfo.is_super == 1) {
+                classifyLists = await this.ctx.model.Classify.find();
+
+            } else {
+                let admin_organ_id = this.ctx.session.adminInfo.organ_id;
+                classifyLists = await this.ctx.model.Classify.find({
+                    organ_id: admin_organ_id
+                });
+            }
+
+        }
+
+        var page = this.ctx.request.query.page || 1;
+        var pageSize = 10;
+        var allLists = await this.ctx.model.Classify.aggregate([
+            findJson1,
+            findJson2,
+            matchJson
+        ]);
+        var totalNum = allLists.length;
+        if (page > Math.ceil(totalNum / pageSize)) {
+            page = Math.ceil(totalNum / pageSize)
+        }
+        if (page <= 0) {
+            page = 1;
+        }
+        var lists = await this.ctx.model.Classify.aggregate([
+            findJson1,
+            findJson2,
+            matchJson,
+            {
+                $sort: { add_time: -1 }
+            },
+            { "$skip": (page - 1) * pageSize },
+            { "$limit": pageSize }
+        ]);
+        console.log("lists=========++++++++++");
+        console.log(lists);
+
+        await this.ctx.render('admin/allocate/allocated', {
+
+            lists, exam_id, examLists, classify_id, classifyLists,
+            totalPages: Math.ceil(totalNum / pageSize),
+            page: page,
+            totalNum: totalNum
+        });
+       
 
 	}
 
     //已结束
     async endLists() {
-	
-		await this.ctx.render('admin/allocate/endLists', {
-			
-		});
+        // 大于考试结束时间 exam_end  room_status 分配状态为1
+        let exam_id = this.ctx.request.query.exam_id ? this.app.mongoose.Types.ObjectId(this.ctx.request.query.exam_id) : "";
+        let classify_id = this.ctx.request.query.classify_id ? this.app.mongoose.Types.ObjectId(this.ctx.request.query.classify_id) : "";
+
+        let examLists, classifyLists;
+
+        let findJson1 = {
+            $lookup: {
+                from: 'exam',
+                localField: 'exam_id',
+                foreignField: '_id',
+                as: 'exam'
+            }
+        }
+
+        var findJson2 = {
+            $lookup: {
+                from: "examinee",
+                let: { classify_id: "$_id" },
+                pipeline: [
+                    {
+                        $match:
+                        {
+                            verify_status: 1,
+                            pay_status: 1,
+                            room_status: 1,
+                            $expr: {
+                                $and:
+                                    [
+                                        { $eq: ["$classify_id", "$$classify_id"] }
+                                    ]
+                            },
+                            // $or: [
+                            //     { "brand_name": { "$regex": keyword } },
+                            // ]
+                        }
+                    },
+                    // { $group: { _id: "$pay_status", total: { $sum: "$num" } } }
+                ],
+                as: "examinee_roomed"
+            },
+
+        };
+
+        let matchJson = {
+            $match: {
+                room_status: 1,
+                exam_end: { $lt: new Date() }
+            }
+        }
+        if (exam_id) {
+            matchJson.$match.exam_id = exam_id;
+        }
+        if (classify_id) {
+            matchJson.$match._id = classify_id;
+        }
+        if (this.ctx.session.adminInfo.is_super == 1) {
+            examLists = await this.ctx.model.Exam.find();
+            //classifyLists = await this.ctx.model.Classify.find();
+
+        } else {
+            let admin_organ_id = this.ctx.session.adminInfo.organ_id;
+            examLists = await this.ctx.model.Exam.find({ organ_id: admin_organ_id });
+            //classifyLists = await this.ctx.model.Classify.find({organ_id:admin_organ_id});
+
+            matchJson.$match.organ_id = this.app.mongoose.Types.ObjectId(admin_organ_id);
+
+
+        }
+        if (exam_id) {
+            classifyLists = await this.ctx.model.Classify.find({ exam_id: exam_id });
+        } else {
+            if (this.ctx.session.adminInfo.is_super == 1) {
+                classifyLists = await this.ctx.model.Classify.find();
+
+            } else {
+                let admin_organ_id = this.ctx.session.adminInfo.organ_id;
+                classifyLists = await this.ctx.model.Classify.find({
+                    organ_id: admin_organ_id
+                });
+            }
+
+        }
+
+        var page = this.ctx.request.query.page || 1;
+        var pageSize = 10;
+        var allLists = await this.ctx.model.Classify.aggregate([
+            findJson1,
+            findJson2,
+            matchJson
+        ]);
+        var totalNum = allLists.length;
+        if (page > Math.ceil(totalNum / pageSize)) {
+            page = Math.ceil(totalNum / pageSize)
+        }
+        if (page <= 0) {
+            page = 1;
+        }
+        var lists = await this.ctx.model.Classify.aggregate([
+            findJson1,
+            findJson2,
+            matchJson,
+            {
+                $sort: { add_time: -1 }
+            },
+            { "$skip": (page - 1) * pageSize },
+            { "$limit": pageSize }
+        ]);
+        console.log("lists=========++++++++++");
+        console.log(lists);
+
+        await this.ctx.render('admin/allocate/endLists', {
+
+            lists, exam_id, examLists, classify_id, classifyLists,
+            totalPages: Math.ceil(totalNum / pageSize),
+            page: page,
+            totalNum: totalNum
+        });
+   
 
 	}
 
